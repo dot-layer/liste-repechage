d73a5de80372db2f3e07c80e6c5a540b904d16d3
Laurent Caron <laurent.m.caron@gmail.com>
2019-06-15 18:32:53 -0400

    Ajout de la moyenne corrigée (rang = 33 lorsque pas dans la liste)

--- test-baseline.R
+++ test-baseline.R
@@ -19,5 +19,38 @@ by_player <-
                         min_ranking = min(rank),
                         max_ranking = max(rank),
                         variance = var(rank),
-                        count = n()),
+                        count = n(),
+                        median = median(rank),
+                        corrected_average = (sum(rank) + (14 - count) * 33) / 14) %>% 
+              mutate(final_selection = rank(corrected_average)),
             by = "ID")
+
+
+# player_pairs <- expand.grid(by_player$ID,
+#                             by_player$ID) %>% 
+#   unique() %>% 
+#   rename(ID1 = Var1, ID2 = Var2) %>% 
+#   filter(ID1 != ID2)
+# 
+# rank_comparison <- function(ranker, df){
+#   rank1 <- tibble(by_player$ID) %>% 
+#     left_join()
+#     df %>%
+#     select(ID1 = ID, rank1 = rank)
+#   rank2 <- df %>%
+#     select(ID2 = ID, rank2 = rank)
+#   
+#   merge(rank1, rank2, all = TRUE) %>% 
+#     unique() %>% 
+#     filter(ID1 != ID2) %>% 
+#     mutate(rank1_lt_rank2 = (rank1 < rank2)) %>% 
+#     mutate(ranker = ranker)
+# }
+# 
+# results <- map_df(unique(main$ranker),
+#                   ~ rank_comparison(.x, main %>% filter(ranker == .x)))
+# 
+# results %<>% group_by(ID1) %>% summarise(mean_beat = mean(rank1_lt_rank2),
+#                                          count_beat = sum(rank1_lt_rank2))
+# 
+# by_player %<>% left_join(results, by = c("ID" = "ID1"))
